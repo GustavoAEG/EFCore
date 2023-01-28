22792151dcb8a624c31b5ffa88f55648fd49702c
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-28 15:28:40 -0300

    Adicionar arquivos de projeto.

---
+++ EFCore/Context/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_EF.Models;
+
+namespace EFCore.Context
+{
+    public class DbContext
+    {
+        private DbContextOptions<ApplicationDbContext> contextOptions;
+
+        public DbContext(DbContextOptions<ApplicationDbContext> contextOptions)
+        {
+            this.contextOptions = contextOptions;
+        }
+    }
+}
