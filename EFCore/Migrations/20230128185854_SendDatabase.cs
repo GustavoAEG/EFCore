ab78213fb5f956486f437a8c618fa497d688fb9e
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-28 15:28:40 -0300

    #001 - Scaffolding CRUD and Index

---
+++ EFCore/Migrations/20230128185854_SendDatabase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class SendDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          migrationBuilder.Sql("INSERT INTO Alunos(Email, Nascimento, Nome, Sexo) VALUES('gustavo@gmail.com','17/12/1212','Guustavo','Masculino')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Alunos");
+        }
+    }
+}
