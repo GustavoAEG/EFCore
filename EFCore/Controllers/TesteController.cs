20f12bb74a8ecdf442ce0c194f172c426f4a0387
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-29 20:37:01 -0300

    #003 - Edit Scaffolding

--- EFCore/Controllers/TesteController.cs
+++ EFCore/Controllers/TesteController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using EFCore.Context;
 using MVC_EF.Models;
 
 namespace EFCore.Controllers
@@ -14,10 +16,42 @@ namespace EFCore.Controllers
 
         public IActionResult Index()
         {
-            var alunos = _context.Alunos.ToList();
-           // var aluno = _context.Alunos.OrderBy(a => a.Nome);
+            //var alunos = _context.Alunos.ToList();
+           var alunos = _context.Alunos.OrderBy(a => a.Nome);
 
+            return View(alunos);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Nome,Sexo,Email,Nascimento")]Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aluno);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var aluno = _context.Alunos.SingleOrDefault(a => a.Id == id);
+            if (aluno == null)
+            {
+
+            }
+
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,7 @@ namespace EFCore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Teste}/{action=Index}/{id?}");
             });
         }
     }
