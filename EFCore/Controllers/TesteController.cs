53502dc7f3a1153fccb54c064ef518b209659087
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-29 13:08:11 -0300

    #002 - CRUD views with Scafolding

---
+++ EFCore/Controllers/TesteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_EF.Models;
+
+namespace EFCore.Controllers
+{
+    public class TesteController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public TesteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var alunos = _context.Alunos.ToList();
+           // var aluno = _context.Alunos.OrderBy(a => a.Nome);
+
+            return View();
+        }
+    }
+}
