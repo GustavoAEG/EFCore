1daf28c09b1d4fab959bbf00c882f670640da1c0
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-02-02 20:13:23 -0300

    #009 - Injeção de dependencia AlunoTipoSocio

--- EFCore/Controllers/AlunoTipoSocioController.cs
+++ EFCore/Controllers/AlunoTipoSocioController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using EFCore.Models;
+using MVC_EF.Models;
 
 namespace EFCore.Controllers
 {
     public class AlunoTipoSocioController : Controller
     {
+        private ApplicationDbContext contexto; //pega os dados do banco e faz a injecao de dependencia
+
+        public AlunoTipoSocioController(ApplicationDbContext _applicationDbContext)
+        {
+                contexto = _applicationDbContext;//Injecao de dependencia
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var infoAluno = contexto.Alunos.ToList
+            return View(infoAluno);
         }
     }
 }
